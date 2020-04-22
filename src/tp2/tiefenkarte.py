30f3bad40341de3c36e39b98256ae954d2b6e7c2
Max Pfaller <pfaller.max@t-online.de>
2020-04-22 16:57:59 +0200

    VSCode Commit Test

--- src/tp2/tiefenkarte.py
+++ src/tp2/tiefenkarte.py
@@ -3,7 +3,9 @@ import os
 import sys
 import numpy as np
 import cv2
-
+import carla
+import random
+import time
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -13,37 +15,37 @@ try:
 except IndexError:
     pass
 
-import carla
 
-import random
-import time
+
 
 IM_WIDTH = 800
 IM_HEIGTH = 600
 
 
-def myprocess(image):
+def processimage(image):
+    xrange = np.array([390,410])
+    yrange = np.array([390,410])
     # Data processing
     array = np.array(image.raw_data)
     i = array.reshape(IM_HEIGTH,IM_WIDTH,4)
     i2 = i[:,:,:3]
-    x = i2[400,400]
+    pixel = i2[400,400]
 
     # Entfernung aus GBR (nicht RGB!) Daten berechnen
-    normalized = (x[2] + x[1] * 256 + x[0] * 256 * 256) / (256 * 256 * 256 - 1)
+    normalized = (pixel[2] + pixel[1] * 256 + pixel[0] * 256 * 256) / (256 * 256 * 256 - 1)
     in_meters = 1000 * normalized
 
+    poi = i2[290:510, 290:510]
+    poi2 = cv2.cvtColor(poi, cv2.COLOR_BGR2GRAY)
+    gaus = cv2.adaptiveThreshold(poi2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115,1)
     #print('distance of pixel at frame ',image.frame_number, ' is ', in_meters, ' meters')
 
     # Visualisierung des Pixel of Interest
-    blue = [255,0,0]
-    for h in range(390,410):
-        for w in range(390,410):
-            i2[h,w] = blue
-
+    #blue = [255,0,0]
+    #i2[390:410, 390:410] = blue
     
     # Graphische Ausgabe des Sensorbilds
-    cv2.imshow('', i2)
+    cv2.imshow('', gaus)
     cv2.waitKey(1)
     #speed(in_meters)
     return in_meters
@@ -81,21 +83,21 @@ try:
     vehicle_2 = world.spawn_actor(bp_2, spawn_point_bp_2)
     #vehicle_1.set_autopilot(True)
     vehicle_1.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    vehicle_2.apply_control(carla.VehicleControl(throttle=0.8, steer=0.1))
+    vehicle_2.apply_control(carla.VehicleControl(throttle=0.8, steer=0.0))
     actor_list.append(vehicle_1)
     actor_list.append(vehicle_2)
 
-    cam_bp_2 = blueprint_library.find('sensor.camera.depth')
+    cam_bp_2 = blueprint_library.find('sensor.camera.rgb')
     #cam_bp_2.set_attribute('fov', '110')
     cam_bp_2.set_attribute('image_size_x', '800')
     cam_bp_2.set_attribute('image_size_y', '600')
     transform = carla.Transform(carla.Location(x=1.5, z=2.4))
     sensor = world.spawn_actor(cam_bp_2, transform, attach_to=vehicle_2)
+    
 
     cc = carla.ColorConverter.Depth
     #sensor.listen(lambda image: image.save_to_disk('output/%06d.png' % image.frame, cc))
-    sensor.listen(lambda image: (myprocess(image)))
-
+    sensor.listen(lambda image: (processimage(image)))
 
     time.sleep(50)
 
