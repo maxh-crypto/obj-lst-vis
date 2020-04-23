ab94939d2d1b08ac17b8a7ce4ae67c53be6dae1f
maxh-crypto <max-haindl@t-online.de>
2020-04-23 14:26:32 +0200

    made changes in bagselector_widget

--- src/rqt_postproc/src/rqt_postproc/bagselector_widget.py
+++ src/rqt_postproc/src/rqt_postproc/bagselector_widget.py
@@ -1,5 +1,6 @@
+import os
 from python_qt_binding import QtCore, QtGui
-from python_qt_binding.QtWidgets import QWidget, QLineEdit, QPushButton, QGridLayout
+from python_qt_binding.QtWidgets import QWidget, QLineEdit, QPushButton, QGridLayout, QFileDialog
 
 class BagSelectorWidget(QWidget):    
     def __init__(self):
@@ -8,7 +9,7 @@ class BagSelectorWidget(QWidget):
         # create elements
         self.bag1Edit = QLineEdit()
         self.bag1Btn = QPushButton("bag file 1")
-        self.bag1Btn.clicked.connect(btn1_clicked)
+        self.bag1Btn.clicked.connect(self.btn1_clicked)
         self.bag2Edit = QLineEdit()
         self.bag2Btn = QPushButton("bag file 2")
         # self.bag2Btn.clicked.connect(btn2_clicked)
@@ -23,5 +24,7 @@ class BagSelectorWidget(QWidget):
         self.layout.addWidget(self.bag2Btn, 2, 2)
         self.setLayout(self.layout)
 
-    def btn1_clicked():
-        __fileName1 = QFileDialog.getOpenFileName(self, 'Select file', 'C:\\', "Bag files (*.bag)")
+    def btn1_clicked(self):
+	cwd = os.getcwd() # current working directory
+        self.__fileName1 = QFileDialog.getOpenFileName(self, 'Select file', cwd, "Bag files (*.bag)")
+	self.bag1Edit.setText(self.__fileName1)
