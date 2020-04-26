aac204f27b915b0b54a7598fc3fcf18b485492f0
TWagner604 <TWagner604@github.com>
2020-04-26 14:02:12 +0200

    RecordBagfile funktioniert, und LaunchFile staret automatische Generierung eines BagFile

--- src/tp3/scripts/RecordBagfile.py
+++ src/tp3/scripts/RecordBagfile.py
@@ -6,32 +6,32 @@ from object_list.msg import ObjectsList
 from object_list.msg import ObjectList
 
 import rospy
+import rosnode
 import math
-
-topic = 'visualization_marker_array'
+import rosbag
+import datetime
+import os
 
 rospy.init_node('Record_Objektlist')
-  
- 
-d
+root_path = os.environ['HOME'] + '/obj-lst-vis/src/tp3/bagfiles/' 
+current_time = rospy.Time
+path  = root_path + 'Objektliste_' + str(datetime.datetime.now()) + '.bag'
+rospy.loginfo (datetime.datetime.now())
+
+bag = rosbag.Bag(path,'w')
 
 def callback(data):
-   
-    markerArray = MarkerArray()
-    rospy.loginfo(data.obj_list[0].geometric)
-    for i in range(len(data.obj_list)):
-        markerObj = evaluateObject(data.obj_list[i])
-        #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.obj_list[i])
-        markerObj.id = i
-        markerArray.markers.append(markerObj)
-
-    publisher.publish(markerArray)
+    
+    bag.write('camera_obj',data)
    
 def listener():
 
     rospy.Subscriber("camera_obj", ObjectsList, callback)
     # spin() simply keeps python from exiting until this node is stopped
+    
+    
     rospy.spin()
+    bag.close()
 
 if __name__ == '__main__':
     listener()
