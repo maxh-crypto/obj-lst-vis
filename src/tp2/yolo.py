2a54cbaeb41a507902f135c5ecfd6b57fba9af41
der0817 <der0817@thi.de>
2020-05-03 14:09:46 +0200

    Changes in scenario, command line, fusion tiefenkarte+ yolo and variables for objlist

--- src/tp2/yolo.py
+++ src/tp2/yolo.py
@@ -30,13 +30,12 @@ IM_HEIGTH = 600
 
 i1 = 0
 
-#test3
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 #ap.add_argument("-i", "--image", required=True,
 #   help="path to input image")
-ap.add_argument("-y", "--yolo", required=True,
-    help="base path to YOLO directory")
+#ap.add_argument("-y", "--yolo", required=True,
+#    help="base path to YOLO directory")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
     help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3,
@@ -44,6 +43,7 @@ ap.add_argument("-t", "--threshold", type=float, default=0.3,
 args = vars(ap.parse_args())
 
 # load the COCO class labels our YOLO model was trained on
+args["yolo"] = "yolo-coco"
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
@@ -60,19 +60,33 @@ configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
+def test(x,z):
+    global b1
+    #global b2
+    global detectionName 
+    b1 = x
+    #b2 = y
+    detectionName = z
+
+def objSum(number):
+    global objNumber
+    objNumber = number
 
 
 def myprocess(image):
     # loop over the frames from the video stream
-    print(image.frame_number)
+    #print(image.frame_number)
+
+    #test(24)
+    #print(z)
+
     array = np.array(image.raw_data)
-    i = array.reshape(IM_HEIGTH,IM_WIDTH,4)
-    frame = i[:,:,:3]
+    i2 = array.reshape(IM_HEIGTH,IM_WIDTH,4)
+    frame = i2[:,:,:3]
     # Graphische Ausgabe des Sensorbilds
 
     (H, W) = frame.shape[:2]
     
-
     # determine only the *output* layer names that we need from YOLO
     ln = net.getLayerNames()
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
@@ -132,51 +146,72 @@ def myprocess(image):
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
         args["threshold"])
 
+
+    j = [0]*400
+    s = [""]*400
+    objSum(len(idxs))
+
     # ensure at least one detection exists
     if len(idxs) > 0:
+        #print(len(idxs))
+        #objSum(len(idxs))
         # loop over the indexes we are keeping
         for i in idxs.flatten():
+            #i=i-1
+            #print(i)
             # extract the bounding box coordinates
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
-            print(LABELS[classIDs[i]])
-            print(boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3])
+            #(x, y, w, h)
+            #print((x, y),(w, h))
+            s[i] = [LABELS[classIDs[i]]]
+            #j = [(x, y), (w, h)]
+            j[i] = [boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]]
+            #test((x, y), (w, h), s)
+            test(j, s)
+            #print(LABELS[classIDs[i]]) 
+                
+            #print(LABELS[classIDs[i]], "at" , boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3])
 
-            i1 = boxes[i][0]
 
-            # draw a bounding box rectangle and label on the image
-            #color = [int(c) for c in COLORS[classIDs[i]]]
-            #cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            #text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-            #cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-            #    0.5, color, 2)
     else:
         print("none")
-    # show the output image
-    #cv2.imshow("Image", frame)
-    #cv2.waitKey(1)
-    #cv2.destroyAllWindows()
 
-    #frame = pyautogui.screenshot(region=(60, 20, 1280, 800))
-    #frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
-    #frame = imutils.resize(frame, width=400)
-
-    # grab the frame dimensions and convert it to a blob
-    #(h, w) = frame.shape[:2]
-    #blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-    #    0.007843, (300, 300), 127.5)
-
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    #net.setInput(blob)
-    #detections = net.forward()
-
-
-    #cv2.imshow('bild', i2)
-    #cv2.waitKey(10) 
-    return image
+    #print(image.frame_number)    
+    #test(boxes, 1, LABELS)
+    return frame
     #cv2.destroyAllWindows()
     
+
+def myprocess2(image2):
+    xrange = np.array([390,410])
+    yrange = np.array([390,410])
+    # Data processing
+    array = np.array(image2.raw_data)
+    i = array.reshape(IM_HEIGTH,IM_WIDTH,4)
+    i2 = i[:,:,:3]
+    pixel = i2[398,344] #400,400
+
+
+    # Entfernung aus GBR (nicht RGB!) Daten berechnen
+    normalized = (pixel[2] + pixel[1] * 256 + pixel[0] * 256 * 256) / (256 * 256 * 256 - 1)
+    in_meters = 1000 * normalized
+
+    poi = i2[290:510, 290:510]
+    poi2 = cv2.cvtColor(poi, cv2.COLOR_BGR2GRAY)
+    gaus = cv2.adaptiveThreshold(poi2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115,1)
+    print('distance of pixel at frame ',image2.frame_number, ' is ', in_meters, ' meters')
+
+    # Visualisierung des Pixel of Interest
+    #blue = [255,0,0]
+    #i2[390:410, 390:410] = blue
+    
+    # Graphische Ausgabe des Sensorbilds
+    #cv2.imshow('gaus', i2)
+    #cv2.waitKey(25)
+    #cv2.destroyAllWindows()
+    #speed(in_meters)
+    return in_meters
 
 actor_list = []
 
@@ -205,16 +240,20 @@ try:
     # Koordination durch Probieren
     # spawn_point = random.choice(world.get_map().get_spawn_points())
     spawn_point_bp_1 = carla.Transform(carla.Location(x=130,y=-6,z=1), carla.Rotation(yaw=180))
-    spawn_point_bp_2 = carla.Transform(carla.Location(x=140,y=-6,z=1), carla.Rotation(yaw=180))
+    spawn_point_bp_2 = carla.Transform(carla.Location(x=150,y=-6,z=1), carla.Rotation(yaw=180))
+    spawn_point_bp_3 = carla.Transform(carla.Location(x=120,y=-3.5,z=1), carla.Rotation(yaw=180))
     vehicle_1 = world.spawn_actor(bp_1, spawn_point_bp_1)
     vehicle_2 = world.spawn_actor(bp_2, spawn_point_bp_2)
+    vehicle_3 = world.spawn_actor(bp_2, spawn_point_bp_3)
     #vehicle_1.set_autopilot(True)
     vehicle_1.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    vehicle_2.apply_control(carla.VehicleControl(throttle=0.8, steer=0.0))
+    vehicle_2.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
+    vehicle_3.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
     actor_list.append(vehicle_1)
     actor_list.append(vehicle_2)
+    actor_list.append(vehicle_3)
 
-    cam_bp_2 = blueprint_library.find('sensor.camera.rgb')
+    cam_bp_2 = blueprint_library.find('sensor.camera.rgb') #rgb
     #cam_bp_2.set_attribute('fov', '110')
     cam_bp_2.set_attribute('image_size_x', '800')
     cam_bp_2.set_attribute('image_size_y', '600')
@@ -222,15 +261,40 @@ try:
     transform = carla.Transform(carla.Location(x=1.5, z=2.4))
     sensor = world.spawn_actor(cam_bp_2, transform, attach_to=vehicle_2)
 
+    cam_bp_3 = blueprint_library.find('sensor.camera.depth') #rgb
+    #cam_bp_2.set_attribute('fov', '110')
+    cam_bp_3.set_attribute('image_size_x', '800')
+    cam_bp_3.set_attribute('image_size_y', '600')
+    cam_bp_3.set_attribute('sensor_tick', '0.5')
+    transform2 = carla.Transform(carla.Location(x=1.5, z=2.4))
+    sensor2 = world.spawn_actor(cam_bp_3, transform2, attach_to=vehicle_2)
+
     #cc = carla.ColorConverter.rgb
     #sensor.listen(lambda image: image.save_to_disk('output/%06d.png' % image.frame, cc))
     
 
     sensor.listen(lambda image: (myprocess(image)))
+    time.sleep(0.6)
+    sensor2.listen(lambda image2: (myprocess2(image2)))   
 
 
     #while True:
-        #print(i1)
+    #time.sleep(0.5)
+    while True:
+        time.sleep(0.5)
+        #print("ok2")
+        #print(b1[0][0])
+        for i in range(objNumber):
+            #print(i)
+            #i = i-1
+            print(b1[i])
+            a = b1[i][0] + b1[i][2]*0.5
+            print(a)
+            print(detectionName[i])
+        #print(b1[0][0],b1[0][1])
+        #print(b2)
+        #print(objNumber)
+        #print(detectionName[0], "2")
 
     time.sleep(55)
 
