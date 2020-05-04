5ca236313981ac32457204d7eb56413eb16470d0
Christoph Zach <christoph@zach-fw.de>
2020-05-04 16:11:40 +0200

    version 04.05.2020: added prototype getAdvancedData()

--- src/tp3/scripts/Rosbag_Analysis_main.py
+++ src/tp3/scripts/Rosbag_Analysis_main.py
@@ -12,6 +12,10 @@ print(object_count)
 (array_timestamps, array_values_x) = ra.Rosbag_Analysis.getRawData('../bagfiles/Objektliste.bag', 0, "dimension", "length")
 (array_timestamps, array_values_y) = ra.Rosbag_Analysis.getRawData('../bagfiles/Objektliste.bag', 0, "classification", "car")
 
+#prototype
+(array_timestamps, array_values_difference) = ra.Rosbag_Analysis.getAdvancedData('../bagfiles/Objektliste.bag', '../bagfiles/Objektliste.bag', 0, 1, "dimension", "width", "difference")
+
+
 #deprecated:
 # example: getting geometric.x values for simple plot
 x_array_obj1 = []   # array of all x values of object 1
@@ -36,7 +40,9 @@ print("Printing  values out of generic function: ")
 for i in range(0, len(array_values_y), 1):
     print(array_values_y[i])
 
-
+print("Printing  values out of advanced function - difference: ")
+for i in range(0, len(array_values_difference), 1):
+    print(array_values_difference[i])
   
 # for calculating differnces between arrays:
   
