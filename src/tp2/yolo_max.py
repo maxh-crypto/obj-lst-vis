0b6796a2fdc157e20bd1971bb8d076f4840291fb
Pfixam <62438207+Pfixam@users.noreply.github.com>
2020-05-15 17:12:48 +0200

    check if array is just int

--- src/tp2/yolo_max.py
+++ src/tp2/yolo_max.py
@@ -28,17 +28,13 @@ import time
 IM_WIDTH = 800
 IM_HEIGTH = 600
 
-i1 = 0
-
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", required=True,
-#   help="path to input image")
 #ap.add_argument("-y", "--yolo", required=True,
 #    help="base path to YOLO directory")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
+ap.add_argument("-c", "--confidence", type=float, default=0.5, #0.9
     help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
+ap.add_argument("-t", "--threshold", type=float, default=0.4, #0.6
     help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
@@ -60,177 +56,201 @@ configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-def test(x,z):
-    global b1
-    #global b2
-    global detectionName 
-    b1 = x
-    #b2 = y
-    detectionName = z
+def arrayofdepthcamera(imagedepth):
+    global depthimage
+    depthimage = imagedepth
+
+def yoloVariables(x, y, z):
+    global objCoordinates
+    global objConfidence
+    global objDetectionName 
+    objCoordinates = x
+    objConfidence = y
+    objDetectionName = z
 
 def objSum(number):
     global objNumber
     objNumber = number
 
-def bwframe(bwframe):
-    global frame
-    frame = bwframe
+def distance(number1):
+    global objDistance
+    objDistance = number1
 
-def dist(number1):
-    global distance
-    distance = number1
+#def bwframe(bwframe):
+    #global frame23
+    #frame23 = bwframe
 
+def count(a):
+    global counter
+    counter =a
+
+def process1(b):
+    global step1
+    step1 = b
+
+def process2(c):
+    global step2
+    step2 = c
+
+def process3(d):
+    global step3
+    step3 = d
+
+def getdepth(array):
+    if type(array) == int:
+        distance(0)
+    else:
+        yhalf = array[0] + (array[2] * 0.5)
+        xhalf = array[1] + (array[3] * 0.5)
+
+        pixel = depthimage[int(xhalf),int(yhalf)]
+        normalized = (pixel[2] + pixel[1] * 256 + pixel[0] * 256 * 256) / (256 * 256 * 256 - 1)
+        in_meters = 1000 * normalized
+        distance(in_meters)
 
 def myprocess(image):
-    # loop over the frames from the video stream
-    #print(image.frame_number)
 
-    #test(24)
-    #print(z)
-
+    #Configurate the RGB-Image and starting the yolo-function
     array = np.array(image.raw_data)
     i2 = array.reshape(IM_HEIGTH,IM_WIDTH,4)
     frame = i2[:,:,:3]
-    # Graphische Ausgabe des Sensorbilds
+    yolostart(frame)
 
-    (H, W) = frame.shape[:2]
-    
-    # determine only the *output* layer names that we need from YOLO
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return frame 
 
-    # construct a blob from the input image and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes and
-    # associated probabilities
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-        swapRB=True, crop=False)
-    net.setInput(blob)
-    start = time.time()
-    layerOutputs = net.forward(ln)
-    end = time.time()
-    
-    # show timing information on YOLO
-    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+def yolostart(imagevariable):
+    if step1 > 1:
+        process1(0)
+        frame = imagevariable
+        (H, W) = imagevariable.shape[:2]
+        
+        # determine only the *output* layer names that we need from YOLO
+        #ln = net.getLayerNames()
+        #ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        ln = net.getUnconnectedOutLayersNames()
 
-    # initialize our lists of detected bounding boxes, confidences, and
-    # class IDs, respectively
-    boxes = []
-    confidences = []
-    classIDs = [] 
+        # construct a blob from the input image and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes and
+        # associated probabilities
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+            swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
+        
+        # show timing information on YOLO
+        print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-    # loop over each of the layer outputs
-    for output in layerOutputs:
-        # loop over each of the detections
-        for detection in output:
-            # extract the class ID and confidence (i.e., probability) of
-            # the current object detection
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
+        # initialize our lists of detected bounding boxes, confidences, and
+        # class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = [] 
 
-            # filter out weak predictions by ensuring the detected
-            # probability is greater than the minimum probability
-            if confidence > args["confidence"]:
-                # scale the bounding box coordinates back relative to the
-                # size of the image, keeping in mind that YOLO actually
-                # returns the center (x, y)-coordinates of the bounding
-                # box followed by the boxes' width and height
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
+        # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability) of
+                # the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
 
-                # use the center (x, y)-coordinates to derive the top and
-                # and left corner of the bounding box
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > args["confidence"]:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
 
-                # update our list of bounding box coordinates, confidences,
-                # and class IDs
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
 
-    # apply non-maxima suppression to suppress weak, overlapping bounding
-    # boxes
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-        args["threshold"])
-
-
-    j = [0]*400
-    s = [""]*400
-    objSum(len(idxs))
-
-    # ensure at least one detection exists
-    if len(idxs) > 0:
-        #print(len(idxs))
-        #objSum(len(idxs))
-        # loop over the indexes we are keeping
-        for i in idxs.flatten():
-            #i=i-1
-            #print(i)
-            # extract the bounding box coordinates
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            #(x, y, w, h)
-            #print((x, y),(w, h))
-            s[i] = [LABELS[classIDs[i]]]
-            #j = [(x, y), (w, h)]
-            j[i] = [boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]]
-            #test((x, y), (w, h), s)
-            test(j, s)
-            #print(LABELS[classIDs[i]]) 
-                
-            #print(LABELS[classIDs[i]], "at" , boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3])
-
-            forblackwhiteframe = frame[(boxes[i][0]):(boxes[i][0]+boxes[i][2]), (boxes[i][1]):(boxes[i][1]+boxes[i][3])]
-            frameingrey = cv2.cvtColor(forblackwhiteframe, cv2.COLOR_BGR2GRAY)
-            blackwhite = cv2.adaptiveThreshold(frameingrey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115,1)
-            bwframe(blackwhite)
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
 
 
-    else:
-        print("none")
+        if counter > 0 :            
+            # apply non-maxima suppression to suppress weak, overlapping bounding
+            # boxes
+            idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+                args["threshold"])
 
-    #print(image.frame_number)    
-    #test(boxes, 1, LABELS)
-    return frame
-    #cv2.destroyAllWindows()
-    
+            j = [0]*900
+            s = [""]*900
+            k = [0]*900
+            objSum(len(idxs))
 
-def processdistance(image2):
-    # Data processing
-    array = np.array(image2.raw_data)
-    i = array.reshape(IM_HEIGTH,IM_WIDTH,4)
-    i2 = i[:,:,:3]
-    #pixel = i2[398,344] #400,400
-    distance_in_middle = 0
-    count_steps = 0
-    for width in range(b1[0][0],(b1[0][0]+b1[2][0])):
-        for length in range(b1[1][0],(b1[1][0]+b1[3][0])):
-            poi = frame[width,length]
-            if poi == [0,0,0]:
-                # Entfernung aus GBR (nicht RGB!) Daten berechnen
-                normalized = (poi[2] + poi[1] * 256 + poi[0] * 256 * 256) / (256 * 256 * 256 - 1)
-                in_meters = 1000 * normalized
-                distance_in_middle += in_meters
-                count_steps+=1
-    dist(distance_in_middle/(count_steps))
+            # ensure at least one detection exists
+            if len(idxs) > 0 & len(idxs) < 100:
+
+                # loop over the indexes we are keeping
+                for i in idxs.flatten():
+                    
+                    if i < 10:
+                        # extract the bounding box coordinates
+                        (x, y) = (boxes[i][0], boxes[i][1])
+                        (w, h) = (boxes[i][2], boxes[i][3])
+
+                        s[i] = [LABELS[classIDs[i]]]
+
+                        j[i] = [boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]]
+
+                        k[i]=[confidences[i]]
+                        yoloVariables(j, k, s)
+
+                        
+
+                        process2(1)
+
+            else:
+                print("none")
 
 
-    #poi = i2[290:510, 290:510]
-    #poi2 = cv2.cvtColor(poi, cv2.COLOR_BGR2GRAY)
-    #gaus = cv2.adaptiveThreshold(poi2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115,1)
-    #print('distance of pixel at frame ',image2.frame_number, ' is ', in_meters, ' meters')
+        count(2)        
 
-    # Visualisierung des Pixel of Interest
-    #blue = [255,0,0]
-    #i2[390:410, 390:410] = blue
-    
-    # Graphische Ausgabe des Sensorbilds
-    #cv2.imshow('gaus', i2)
-    #cv2.waitKey(25)
-    #cv2.destroyAllWindows()
-    #speed(in_meters)
-    return in_meters
+    process1(2)
+    return None  
+
+def myprocess2(image2):
+    #Giving deept-Image to depth-function
+    depthAlgo(image2)
+    return None
+
+def depthAlgo(image2):
+    if step2 == 1:
+
+        array = np.array(image2.raw_data)
+        i = array.reshape(IM_HEIGTH,IM_WIDTH,4)
+        i2 = i[:,:,:3]
+        #pixel = i2[398,344]
+        arrayofdepthcamera(i2)
+
+
+            
+        
+        
+        #normalized = (pixel[2] + pixel[1] * 256 + pixel[0] * 256 * 256) / (256 * 256 * 256 - 1)
+        #in_meters = 1000 * normalized
+        #distance(in_meters)
+
+
+        # Entfernung aus GBR (nicht RGB!) Daten berechnen
+        
+
+        process2(0)
+        process3(1)
+    return None    
 
 actor_list = []
 
@@ -243,7 +263,7 @@ try:
     # Blueprints = Fahrzeuge und Fußgänger
     blueprint_library = world.get_blueprint_library()
     #bp_1 = random.choice(blueprint_library.filter('vehicle'))
-    bp_1 = blueprint_library.filter('model3')[0]
+    bp_1 = blueprint_library.filter('low_rider')[0]
     bp_2 = blueprint_library.filter('model3')[0]
 
     if bp_1.has_attribute('color'):
@@ -266,8 +286,8 @@ try:
     vehicle_3 = world.spawn_actor(bp_2, spawn_point_bp_3)
     #vehicle_1.set_autopilot(True)
     vehicle_1.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    vehicle_2.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    vehicle_3.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
+    vehicle_2.apply_control(carla.VehicleControl(throttle=0.6, steer=0.0))
+    vehicle_3.apply_control(carla.VehicleControl(throttle=0.6, steer=0.0))
     actor_list.append(vehicle_1)
     actor_list.append(vehicle_2)
     actor_list.append(vehicle_3)
@@ -287,35 +307,30 @@ try:
     cam_bp_3.set_attribute('sensor_tick', '0.5')
     transform2 = carla.Transform(carla.Location(x=1.5, z=2.4))
     sensor2 = world.spawn_actor(cam_bp_3, transform2, attach_to=vehicle_2)
+  
 
-    #cc = carla.ColorConverter.rgb
-    #sensor.listen(lambda image: image.save_to_disk('output/%06d.png' % image.frame, cc))
-    
+    #inits  
+    count(0)
+    process1(2)
+    process2(0)
+    process3(0)
     #yolo 
     sensor.listen(lambda image: (myprocess(image)))
-    time.sleep(2)
+    #FirstYolorun for Hotfix
+    count(1)
     #depth
-    sensor2.listen(lambda data: (processdistance(data)))
+    sensor2.listen(lambda image2: (myprocess2(image2)))   
 
 
-    #while True:
-    #time.sleep(0.5)
+    #temporaly output
     while True:
-        time.sleep(0.5)
-        #print("ok2")
-        #print(b1[0][0])
-        for i in range(objNumber):
-            #print(i)
-            #i = i-1
-            print(b1[i])
-            a = b1[i][0] + b1[i][2]*0.5
-            print(a)
-            print(detectionName[i])
-            print(distance)
-        #print(b1[0][0],b1[0][1])
-        #print(b2)
-        #print(objNumber)
-        #print(detectionName[0], "2")
+        if step3 == 1:
+            if counter > 1:
+                for i in range(objNumber):
+                    getdepth(objCoordinates[i])
+                    print(objDetectionName[i], "at", objCoordinates[i], "Distance:", objDistance, "Confidence", objConfidence[i])
+
+                process3(0)
 
     time.sleep(55)
 
@@ -324,7 +339,7 @@ try:
 #Anzahl der detek. obj
 
 #pixelcoordinaten
-#b1[i] ganze Zeile, b1[i][0]
+#objCoordinates[i] ganze Zeile, objCoordinates[i][0]
 #i   0 1 2 3
 #1   x y w h
 #2   x y w h
@@ -336,7 +351,13 @@ try:
 #3 truck
 
 #Abstand
-#variable -> distance (bisher nur ein fester Integerwert in m)
+#variable -> objDistance (bisher nur ein fester Integerwert in m)
+
+#Confidence (Yologenauigkeit)
+#0.99...
+#0.83...
+#0.73...
+
 finally:
     for actor in actor_list:
         actor.destroy()
