150cbdf6b6d13723fac4f8d3f6c01995965ba4e7
maxh-crypto <max-haindl@t-online.de>
2020-05-18 19:27:16 +0200

    bugfixes in DataEvaluation

--- src/tp3/src/tp3/test/iou_test.py
+++ src/tp3/src/tp3/test/iou_test.py
@@ -4,9 +4,10 @@ sys.path.append('../')
 import DataEvaluation_module as eval
 
 class RosObject():
-    def __init__(self, x, y, l, w, yaw):
+    def __init__(self, x, y, l, w, yaw=0.0, cls=[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]):
         self.geometric = Geometric(x, y, yaw)
         self.dimension = Dimension(l, w)
+        self.classification = Classification(cls)
 
 class Geometric():
     def __init__(self, x, y, yaw):
@@ -18,12 +19,26 @@ class Dimension():
     def __init__(self, l, w):
         self.length = l
         self.width = w        
+    
+class Classification():
+    def __init__(self, prop_values):
+        self.car = prop_values[0]
+        self.truck = prop_values[1]
+        self.motorcycle = prop_values[2]
+        self.bicycle = prop_values[3]
+        self.pedestrian = prop_values[4]
+        self.stacionary = prop_values[5]
+        self.other = prop_values[6]
 
 B_gt_1 = RosObject(0.0, 0.0, 4.0, 2.0, 0.0)
 B_pr_1 = RosObject(1.0, 1.0, 4.0, 2.0, 0.0)
 
-B_gt_2 = RosObject(0.0, 0.0, 4.0, 2.0, 0.0)
-B_pr_2 = RosObject(1.0, 1.0, 4.0, 2.0, 1.0)
+B_gt_2 = RosObject(8.0, 10.0, 4.0, 2.0, 0.0, [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+B_pr_2 = RosObject(8.0, 10.0, 4.0, 2.0, 1.0)
+
+B_gt_3 = RosObject(16.0, 25.0, 4.0, 2.0, 0.0)
+B_pr_3 = RosObject(4.0, 5.0, 4.0, 2.0, 0.0)
+
 
 ### for simple objects in line
 ## intersection:
@@ -40,8 +55,10 @@ print("calculated union " + str(union))
 iou = eval.iou(B_gt_1, B_pr_1)
 assert(iou == (3.0 / 13.0)), "wrong iou value"
 print("calculated iou " + str(iou))
-
+    
+    
 ### rotated objects
+print("***** test rotated object *****")
 ## intersection:
 intersection = eval.intersection(B_gt_2, B_pr_2)
 # assert(intersection == 3.0), "wrong intersection value"
@@ -56,4 +73,14 @@ print("calculated union " + str(union))
 iou = eval.iou(B_gt_2, B_pr_2)
 # assert(iou == (3.0 / 13.0)), "wrong iou value"
 print("calculated iou " + str(iou))
+
+
+print("***** test for TP/FP/mm *****")
+case, index = eval.det_TP_FP_mm([B_gt_1, B_gt_2, B_gt_3], B_pr_2, 0.5)
+if case == 0: # TP
+    print("TP case found. B_pr matched with B_gt_" + str(index + 1))
+elif case == 1: # FP
+    print("FP case detected.")
+else:
+    print("Mismatch detected with B_gt_" + str(index + 1))
 
